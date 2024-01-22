fa8b76a7c0df30c9538918cf62a4a7fc6ea3d6e4
Süleyman Begic <begic@software.tirol>
2024-01-22 12:21:28 +0100

    add login validator

---
+++ Fahrtenbuch.UI/Validation/LoginInfoValidator.cs
@@ -0,0 +1,13 @@
+﻿using Fahrtenbuch.Data.Models;
+using FluentValidation;
+
+namespace Fahrtenbuch.UI.Validation;
+
+public class LoginInfoValidator : AbstractValidator<LoginInfo>
+{
+    public LoginInfoValidator()
+    {
+        RuleFor(x => x.Email).NotNull().NotEmpty();
+        RuleFor(x => x.Password).NotNull().NotEmpty();
+    }
+}
---
+++ Fahrtenbuch.UI/Validation/RegisterInfoValidator.cs
@@ -0,0 +1,16 @@
+﻿using Fahrtenbuch.Data.Models;
+using FluentValidation;
+
+namespace Fahrtenbuch.UI.Validation;
+
+public class RegisterInfoValidator : AbstractValidator<RegisterInfo>
+{
+    public RegisterInfoValidator()
+    {
+        RuleFor(x => x.FirstName).NotNull().NotEmpty();
+        RuleFor(x => x.LastName).NotNull().NotEmpty();
+        RuleFor(x => x.Email).NotNull().NotEmpty();
+        RuleFor(x => x.LoginPassword).NotNull().NotEmpty().MinimumLength(5);
+        RuleFor(x => x.CheckPassword).Equal(x=> x.LoginPassword).WithMessage("Passwort stimmt nicht überein");
+    }
+}
